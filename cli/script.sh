7f849ae57ac6b739eb8c09e39ffe19a9290ad9b5
threesquared <ben@3sq.re>
2018-07-19 10:29:02 +0100

    Build

---
+++ cli/script.sh
@@ -0,0 +1,14 @@
+set -ex
+
+main() {
+    cross build --target $TARGET
+    cross build --target $TARGET --release
+
+    cross run --target $TARGET
+    cross run --target $TARGET --release
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
