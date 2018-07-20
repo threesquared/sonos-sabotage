32bf0924c971407ea326f069c50f44228fff28c0
threesquared <ben@3sq.re>
2018-07-20 21:43:15 +0100

    Add more options

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# stuxsonos
-[![Build Status](https://travis-ci.com/threesquared/stuxsonos.svg?branch=master)](https://travis-ci.com/threesquared/stuxsonos)
+# stuxsonos [![GitHub release](https://img.shields.io/github/release/threesquared/stuxsonos.svg)](https://github.com/threesquared/stuxsonos/releases) [![Build Status](https://travis-ci.com/threesquared/stuxsonos.svg?branch=master)](https://travis-ci.com/threesquared/stuxsonos)
 
 > "It is the fight between tiger and elephant. If the tiger stands his ground, the elephant will crush him with its mass. But, if he conserves his mobility, he will finally vanquish the elephant, who bleeds from a multitude of cuts."
 >
@@ -7,7 +6,8 @@
 
 ## Description
 
-This CLI tool contains a number of modes designed to take various covert actions to disrupt or disable sonos devices on the local network.
+This CLI tool contains a number of modes designed to take various covert and overt actions to disrupt or disable sonos
+devices on the local network for use in office cyber warfare.
 
 ## Installation
 
@@ -15,15 +15,58 @@ Download the latest binary for your platform from the [releases](https://github.
 
 ## Usage
 
-Get a list of options:
+Get a full list of parameters:
 
 ```sh
 $ ./stuxsonos -h
 ```
 
+You can combine certain modes together and also set various options for others:
+
+```sh
+$ ./stuxsonos -oa -p "Beyonce" -i 1000
+```
+
 ### Modes
 
-`-o, --oldMan`
+`-o, --oldman`
 
-This mode tracks the volume of all devices on the network. If it detects an increase of more than 2% it will reduce the volume of that 
-device by twice the detected increase.
+This mode tracks the volume of all devices on the network. If it detects an increase of more than 5 points it will reduce the volume of that 
+device by 1.3 times the detected increase in percentage points.
+
+`-a, --assassin`
+
+This mode watches the currently playing track and matches the artist or track name to a pattern. If a match is found the track
+will be skipped. Pattern defaults to `Ed Sheeran` if not supplied.
+
+`-d, --dictator`
+
+This mode finds any devices not playing the supplied track and will clear the current queue and play the preferred track.
+Track defaults to Guerrilla Radio by Rage Against the Machine if not supplied.
+
+`-s, --saboteur`
+
+This mode randomly performs operations like muting speakers, skipping/pausing tracks and changing the volume.
+
+`-t, --totalitarian`
+
+This mode will clear the queue of all devices and stop any running tracks it finds.
+
+### Options
+
+`-i <interval>`
+
+The internal tick rate to poll devices and take actions in ms. Defaults to `10000`
+
+`-p <pattern>`
+
+The regex pattern to try and match against playing tracks and artists in assassin mode.
+Defaults to `Ed Sheeran`
+
+`-u <uri>`
+
+The track uri to play in dictator mode.
+
+`-x <ip>`
+
+Only perform actions against a specific device IP address
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ extern crate lazy_static;
 extern crate clap;
 extern crate sonos;
 extern crate schedule_recv;
+extern crate rand;
+extern crate regex;
 
 use std::collections::HashMap;
 use std::sync::Mutex;
 use clap::{App, Arg};
+use rand::prelude::*;
+use regex::Regex;
 
 #[derive(Clone)]
 pub struct SpeakerState {
@@ -21,19 +25,79 @@ fn get_previous_state(ip: std::net::IpAddr) -> Option<SpeakerState> {
     DEVICES.lock().unwrap().get(&ip).cloned()
 }
 
+fn set_state(ip: std::net::IpAddr, state: SpeakerState) {
+    DEVICES.lock().unwrap().insert(ip, state);
+}
+
 fn main() {
     let matches = App::new("stuxsonos")
-        .version("0.1.0")
         .arg(Arg::with_name("interval")
-            .help("The refresh interval to poll devices in ms")
+            .help("The interval to poll for devices in ms")
             .short("i")
             .default_value("10000")
             .takes_value(true)
         )
-        .arg(Arg::with_name("oldMan")
+        .arg(Arg::with_name("pattern")
+            .help("Pattern to match in assassin mode")
+            .short("p")
+            .default_value("Ed Sheeran")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("uri")
+            .help("The URI to play in dictator mode")
+            .short("u")
+            .default_value("x-sonos-spotify:spotify:track:1wsRitfRRtWyEapl0q22o8")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("ip")
+            .help("Device IP address to target")
+            .short("x")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("devices")
+            .help("Print out all devices found on the current network and exit")
+            .short("y")
+            .long("devices")
+        )
+        .arg(Arg::with_name("percent")
+            .help("Percent of the time saboteur mode should take an action")
+            .short("z")
+            .default_value("5")
+            .long("percent")
+        )
+        .arg(Arg::with_name("oldman")
              .help("This mode detects volume increases on devices and turns them down")
              .short("o")
-             .long("oldMan")
+             .long("oldman")
+             .conflicts_with_all(&[
+                "dictator",
+                "totalitarian"
+             ])
+        )
+        .arg(Arg::with_name("assassin")
+            .help("This mode matches specific tracks and eliminates them")
+            .short("a")
+            .long("assassin")
+            .conflicts_with("totalitarian")
+        )
+        .arg(Arg::with_name("dictator")
+            .help("This mode enforces a specific track to be playing")
+            .short("d")
+            .long("dictator")
+            .conflicts_with_all(&[
+                "saboteur",
+                "totalitarian"
+            ])
+        )
+        .arg(Arg::with_name("saboteur")
+            .help("This mode aims to completely disrupt playback")
+            .short("s")
+            .long("saboteur")
+        )
+        .arg(Arg::with_name("totalitarian")
+            .help("This mode clears all queues and stops playing tracks")
+            .short("t")
+            .long("totalitarian")
         )
         .get_matches();
 
@@ -53,27 +117,115 @@ fn main() {
         }
 
         for device in devices.iter() {
-            let current_volume = device.volume().unwrap();
-            println!("Found device {} at {} at {}% volume.", device.name, device.ip, current_volume);
+            println!("Found device {} at {}", device.name, device.ip);
+
+            if matches.is_present("devices") {
+                break
+            }
 
             let previous_state = get_previous_state(device.ip);
 
-            if matches.is_present("oldMan") {
-                if previous_state.is_some() {
-                    let previous_volume = previous_state.unwrap().volume;
-                    let difference = current_volume - previous_volume;
-
-                    if difference > 2 {
-                        println!("Detected increase of {} percent!", difference);
-                        let reduction = previous_volume - (difference * 2);
-                        device.set_volume(reduction).unwrap();
-                    }
-                }
+            if matches.is_present("oldman") {
+                old_man(device, previous_state);
             }
 
-            DEVICES.lock().unwrap().insert(device.ip, SpeakerState {
-                volume: current_volume,
+            if matches.is_present("assassin") {
+                assassin(device, matches.value_of("pattern").unwrap());
+            }
+
+            if matches.is_present("dictator") {
+                dictator(device, matches.value_of("uri").unwrap());
+            }
+
+            if matches.is_present("saboteur") {
+                saboteur(device, matches.value_of("percent").unwrap());
+            }
+
+            if matches.is_present("totalitarian") {
+                totalitarian(device);
+            }
+
+            set_state(device.ip, SpeakerState {
+                volume: device.volume().unwrap(),
             });
         }
+
+        if matches.is_present("devices") {
+            break
+        }
+    }
+}
+
+fn old_man(device: &sonos::Speaker, previous_state: std::option::Option<SpeakerState>) {
+    if previous_state.is_some() {
+        let current_volume = device.volume().unwrap();
+        let previous_volume = previous_state.unwrap().volume;
+        let difference: u8 = current_volume - previous_volume;
+
+        if difference > 5 {
+            let reduction: i8 = current_volume as i8 - (difference as f32 * 1.3) as i8;
+
+            if reduction > 0 {
+                println!("Detected volume increase of {} points! Decreasing to {}", difference, reduction);
+                device.set_volume(reduction as u8).unwrap();
+            }
+        }
+    }
+}
+
+fn assassin(device: &sonos::Speaker, pattern: &str) {
+    if let Ok(track) = device.track() {
+        let regex = Regex::new(pattern.trim()).unwrap();
+
+        if regex.is_match(&track.title) || regex.is_match(&track.artist) {
+            println!("Detected matched track! Assassinating {} {}", track.title, track.artist);
+            device.next().unwrap();
+        }
+    }
+}
+
+fn dictator(device: &sonos::Speaker, uri: &str) {
+    if let Ok(track) = device.track() {
+        if track.uri == uri {
+            return
+        }
+        println!("Device playing forbidden track! {} {}", track.uri, uri);
+
+        device.clear_queue().unwrap();
+
+        match device.play_track(uri) {
+            Ok(_) => println!("Corrected it"),
+            Err(err) => println!("Could not dictate track, {}", err)
+        }
+    }
+}
+
+fn saboteur(device: &sonos::Speaker, percent: &str) {
+    let mut rng = thread_rng();
+    let action_chance = rng.gen_range(0, 100);
+
+    if action_chance > percent.parse::<u32>().unwrap() {
+        let action_choice = rng.gen_range(1, 4);
+
+        match action_choice {
+            1 => device.mute().unwrap(),
+            2 => device.next().unwrap(),
+            3 => device.set_volume(rng.gen_range(0, 100)).unwrap(),
+            4 => device.pause().unwrap(),
+            _ => {},
+        }
+
+        println!("Sabotage device with {}", action_choice);
+    }
+}
+
+fn totalitarian(device: &sonos::Speaker) {
+    let transport_state = device.transport_state().unwrap();
+
+    if transport_state == sonos::TransportState::Playing {
+        println!("Device is playing, shutting it down {}", device.name);
+
+        device.stop().unwrap();
+        device.clear_queue().unwrap();
     }
 }
